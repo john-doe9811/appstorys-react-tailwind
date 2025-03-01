ac50876c4f6368e55085fe6292ec5015f77d17ac
Joshua-Lewis <crystopher55@outlook.com>
2025-03-01 11:33:05 -0800

    reupdate-sidebar

--- src/pages/Sider/index.js
+++ src/pages/Sider/index.js
@@ -12,8 +12,64 @@ const Sidebar = () => {
       <div className="p-6 text-4xl font-bold">
         AppStorys
       </div>
+      <div className="flex-grow">
+        <nav className="flex flex-col gap-1">
+          <div>
 
+            <button
+              onClick={() => setIsCampaignsOpen(!isCampaignsOpen)}
 
+              className="flex items-center gap-2 px-6 py-3 text-lg hover:bg-blue-800 transition duration-200 w-full"
+            >
+              <FontAwesomeIcon icon={faInbox} />
+              Campaigns
+              <FontAwesomeIcon className={`ml-auto transform ${isCampaignsOpen ? 'rotate-180' : ''}`} icon={faChevronRight} />
+            </button>
+            {isCampaignsOpen && (
+              <div className="pl-10">
+                <Link to="/sub-campaign1" className="block text-sm py-2 px-6 hover:bg-blue-800">Sub Campaign 1</Link>
+                <Link to="/sub-campaign2" className="block text-sm py-2 px-6 hover:bg-blue-800">Sub Campaign 2</Link>
+              </div>
+            )}
+          </div>
+
+          <Link to="/integrations" className="flex items-center gap-2 px-6 py-3 text-lg hover:bg-blue-800 transition duration-200">
+            <FontAwesomeIcon icon={faChartBar} />
+            Integrations
+          </Link>
+          <Link to="/audience" className="flex items-center gap-2 px-6 py-3 text-lg hover:bg-blue-800 transition duration-200">
+            <FontAwesomeIcon icon={faUser} />
+            Audience
+          </Link>
+          <Link to="/billing" className="flex items-center gap-2 px-6 py-3 text-lg hover:bg-blue-800 transition duration-200">
+            <FontAwesomeIcon icon={faMobileAlt} />
+            Billing
+          </Link>
+          <Link to="/manage-apps" className="flex items-center gap-2 px-6 py-3 text-lg hover:bg-blue-800 transition duration-200">
+            <FontAwesomeIcon icon={faInbox} />
+            Manage Apps
+          </Link>
+          <Link to="/manage-users" className="flex items-center gap-2 px-6 py-3 text-lg hover:bg-blue-800 transition duration-200">
+            <FontAwesomeIcon icon={faUser} />
+            Manage Users
+          </Link>
+        </nav>
+      </div>
+
+      {/* Bottom Section */}
+      <div className="mt-auto p-6 ">
+        <Link to="/settings" className="flex items-center gap-2 px-6 py-3 text-lg hover:bg-blue-800 transition duration-200 w-full">
+          <FontAwesomeIcon icon={faCog} />
+          Settings
+        </Link>
+        <button
+          onClick={() => alert("Sign Out")}
+          className="flex items-center gap-2 px-6 py-3 text-lg text-blue-100 hover:bg-blue-800 transition duration-200 w-full"
+        >
+          <FontAwesomeIcon icon={faSignOutAlt} />
+          Sign Out
+        </button>
+      </div>
     </div>
   );
 };
